65f7d1db6a20867e67602e36bd7c24dec70648b8
eduardo cannini thome <eduardocanninithome@hotmail.com>
2018-04-19 21:00:21 -0300

    index tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Status.create(name:'Aberta')
-# Status.create(name:'Iniciada')
-# Status.create(name:'Pausada')
-# Status.create(name:'Encerrada')
+Status.create(name:'Aberta')
+Status.create(name:'Iniciada')
+Status.create(name:'Pausada')
+Status.create(name:'Encerrada')
 
-# @company = Company.create(name: 'Tasks')
-# @company.save!
-# @client = Client.create(name: 'Interno', company_id: @company.id)
-# @client.save!
-# Project.create(name:'Tasks - Eduardo', client_id: @client.id)
+@company = Company.create(name: 'Tasks')
+@company.save!
+@client = Client.create(name: 'Interno', company_id: @company.id)
+@client.save!
+Project.create(name:'Tasks - Eduardo', client_id: @client.id)
 
-Category.create(name: 'Simples', hour_value: 40.0)
-Category.create(name: 'Média', hour_value: 80.0)
-Category.create(name: 'Complexa', hour_value: 120.0)
+# Category.create(name: 'Simples', hour_value: 40.0)
+# Category.create(name: 'Média', hour_value: 80.0)
+# Category.create(name: 'Complexa', hour_value: 120.0)
